aeec3090517f18f418fb867f457fd69f0b6850c6
Deckgeld <cristianjbc99@gmail.com>
2023-06-15 11:08:25 -0600

    video 3

--- GymManagerFront/src/app/app.module.ts
+++ GymManagerFront/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModel } from 'src/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModel
   ],
   providers: [],
   bootstrap: [AppComponent]
