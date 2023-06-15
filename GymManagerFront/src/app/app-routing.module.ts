efed926b7a379db4b192e08353227fb50fbab2bf
Deckgeld <cristianjbc99@gmail.com>
2023-06-15 13:53:24 -0600

    video 6

--- GymManagerFront/src/app/app-routing.module.ts
+++ GymManagerFront/src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitLayoutComponent } from './share/init-layout/init-layout.component';
+import { AdminLayoutComponent } from './share/admin-layout/admin-layout.component';
 
 const routes: Routes = [
   //Esta line la generamos nosotros y es para que sign-in sea la pagina por defecto
   { path: '', redirectTo: '/sign-in', pathMatch: 'full'},
-  { path: 'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule) }, 
-  { path: 'inventory', loadChildren: () => import('./pages/inventory/inventory.module').then(m => m.InventoryModule) }, 
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
-  { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) }, 
-  { path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+  //RUTAS que tendran el LAYOUT initLayout
+  { path: '', component: InitLayoutComponent, children:[
+      { path: 'sign-in', loadChildren: () => import('./pages/sign-in/sign-in.module').then(m => m.SignInModule) }, 
+      { path: 'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) },
+    ] 
+  },
+  //RUTAS que tendran el LAYOUT AdminLayout
+  { path: '', component: AdminLayoutComponent, children:[
+      { path: 'inventory', loadChildren: () => import('./pages/inventory/inventory.module').then(m => m.InventoryModule) }, 
+      { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
+      { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) }, 
+      { path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+    ] 
+  },
   //Esta linea tambien la creamos nosotros, es para paginas que no existen
   { path: '**', redirectTo: '/not-found', pathMatch: 'full'}
 ];
