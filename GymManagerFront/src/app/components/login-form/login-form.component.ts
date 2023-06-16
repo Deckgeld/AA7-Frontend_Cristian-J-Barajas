c1fd064aec031dab71e4545c48a1ddef4a5e8386
Deckgeld <cristianjbc99@gmail.com>
2023-06-15 22:24:28 -0600

    video 11

--- GymManagerFront/src/app/components/login-form/login-form.component.ts
+++ GymManagerFront/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
-
+  @Input() isSingUp:boolean = true; 
 }
+ 
