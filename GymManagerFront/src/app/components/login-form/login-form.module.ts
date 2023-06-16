e1f07d7d674b0b72ac7030b431dcb84463b5eb42
Deckgeld <cristianjbc99@gmail.com>
2023-06-15 21:02:42 -0600

    video 10

--- GymManagerFront/src/app/components/login-form/login-form.module.ts
+++ GymManagerFront/src/app/components/login-form/login-form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form.component';
+import { MaterialModel } from 'src/material.module';
 
 
 
@@ -9,7 +10,8 @@ import { LoginFormComponent } from './login-form.component';
     LoginFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModel
   ],
   exports: [
     LoginFormComponent
